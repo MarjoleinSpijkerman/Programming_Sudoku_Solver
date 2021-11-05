79c41d1d748d2f4b9b96eddd00c2eac1b6e1af2c
unknown <marjolein1998spijkerman@gmail.com>
2021-11-05 12:06:04 +0100

    added extra functionality, including reset buttons, colored buttons, information pop up screen et cetera

--- sudoku2.py
+++ sudoku2.py
@@ -16,6 +16,8 @@ from librosa.feature import mfcc
 import librosa
 import random
 import pickle
+import tkinter.font as font
+
 
 root = Tk()
 root.title('Sudoku')
@@ -147,26 +149,43 @@ canvas.create_line((75, 315), (795, 315), width=4)
 canvas.create_line((75, 555), (795, 555), width=4)
 canvas.create_line((75, 795), (795, 795), width=4)
 
+canvas.create_line((75, 155), (795, 155), width=1)
+canvas.create_line((75, 235), (795, 235), width=1)
+canvas.create_line((75, 395), (795, 395), width=1)
+canvas.create_line((75, 475), (795, 475), width=1)
+canvas.create_line((75, 635), (795, 635), width=1)
+canvas.create_line((75, 715), (795, 715), width=1)
+
+
+
 canvas.create_line((75, 75), (75, 795), width=4)
 canvas.create_line((315, 75), (315, 795), width=4)
 canvas.create_line((555, 75), (555, 795), width=4)
 canvas.create_line((795, 75), (795, 795), width=4)
 
+canvas.create_line((153, 75), (153, 795), width=1)
+canvas.create_line((233, 75), (233, 795), width=1)
+canvas.create_line((393, 75), (393, 795), width=1)
+canvas.create_line((473, 75), (473, 795), width=1)
+canvas.create_line((633, 75), (633, 795), width=1)
+canvas.create_line((713, 75), (713, 795), width=1)
 
 canvas.pack()
 
 
-#root.geometry("%dx%d+0+0" % (RWidth, RHeight))
-
 def onClick(i):
+	Default = 'SystemButtonFace'
+	buttons[current_button.current_button_col][current_button.current_button_row]['bg'] = Default
 	current_button.update(int(i[0])-1, int(i[1])-1)
-	
+	buttons[current_button.current_button_col][current_button.current_button_row]['bg'] = '#bcedef'
 	
 for i in range(1, 10):
+	myFont = font.Font(size=9, weight="bold")
 	buttons_column = []
 	for j in range(1, 10):
 		str_pos = str(i) + str(j)
-		b = Button(root, height=4, width=8, text = int(0), command = lambda str_pos=str_pos: onClick(str_pos))
+		b = Button(root, height=4, width=8, text = "", command = lambda str_pos=str_pos: onClick(str_pos))
+		b['font'] = myFont
 		b.place(x=i*80, y=j*80)
 		buttons_column.append(b)
 	buttons.append(buttons_column)
@@ -205,8 +224,10 @@ def record_audio():
 	buttons[current_button.current_button_col][current_button.current_button_row]['text'] = predict
 	sudoku.update_value(current_button.current_button_col, current_button.current_button_row, predict)
 
-button_rec = Button(root, text='Record audio', command=record_audio)
-button_rec.place(x=800, y=400)
+myFont = font.Font(size=18, weight="bold")
+button_rec = Button(root, text='Record audio', font = myFont, command=record_audio)
+#button_rec['font'] = myFont
+button_rec.place(x=900, y=200)
 
 def solve_sudoku():	
 	#column, row
@@ -217,9 +238,10 @@ def solve_sudoku():
 	for i in range(9):
 		for j in range(9):
 			buttons[i][j]['text'] = sudoku.return_value(j, i)
-	
-button_solve = Button(root, text='Solve sudoku', command=solve_sudoku)
-button_solve.place(x=800, y=500)
+			
+myFont = font.Font(size=18, weight="bold")	
+button_solve = Button(root, text='Solve sudoku', font=myFont, command=solve_sudoku)
+button_solve.place(x=900, y=540)
 
 value = StringVar()
  
@@ -227,9 +249,68 @@ def submit():
 	number = value.get()
 	buttons[current_button.current_button_col][current_button.current_button_row]['text'] = number
 	sudoku.update_value(current_button.current_button_col, current_button.current_button_row, number)
- 
-value_entry = Entry(root,textvariable = value, font=('calibre',10,'normal')).place(x=900, y=450)
-sub_btn=Button(root,text = 'Manual override', command = submit).place(x=920, y=480)
+
+myFont = font.Font(size=18, weight="bold")
+value_entry = Entry(root,textvariable = value, font = myFont).place(x=880, y=260)
+sub_btn=Button(root,text = 'Manual override', font = myFont, command = submit).place(x=900, y=300)
+
+def reset_one():
+	buttons[current_button.current_button_col][current_button.current_button_row]['text'] = ''
+	sudoku.update_value(current_button.current_button_col, current_button.current_button_row, 0)
+	
+
+def reset_all():
+	for i in range(9):
+		for j in range(9):
+			buttons[i][j]['text'] = ''
+			sudoku.update_value(i, j, 0)
+
+myFont = font.Font(size=18, weight="bold")	
+button_reset_1 = Button(root, text='Reset current button', font=myFont, command=reset_one)
+button_reset_1.place(x=900, y=420)
+	
+button_reset_all = Button(root, text='Reset sudoku', font=myFont, command=reset_all)
+button_reset_all.place(x=900, y=480)
+
+
+
+
+Information_Text = """How to use the sudoku solver:
+
+First select a square in the sudoku --> the currently selected square will turn blue
+Then either select record audio to insert a number between 1 and 9 or manually insert a number
+
+Click on reset current button, to empty the current square
+Click on reset all to empty the entire sudoku
+
+Click on solve sudoku to get a solution
+Please note:
+  -  If there are multiple solutions, the solver will return only 1 solution
+  -  If there are errors in the given input, the solver will not be able to find a good solution"""
+
+def clickAbout():
+    toplevel = Toplevel()
+    label1 = Label(toplevel, text=Information_Text, height=0, width=100)
+    label1.pack()
+
+
+button1 = Button(root, text="How to use the sudoku solver", font=myFont, command=clickAbout)
+button1.place(x=900, y=100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
